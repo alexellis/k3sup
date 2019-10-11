2a2a3a3ba30fbad566193280a729d1a071e54800
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-10-11 09:56:36 +0100

    Fetch helm and use custom home-dir
    Fetches helm for the user, so they don't have to know about it.
    
    Uses a custom home-dir not to interfere with existing helm
    usage.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- pkg/cmd/install_test.go
+++ pkg/cmd/install_test.go
@@ -112,3 +112,12 @@ func Test_RewriteKubeconfig(t *testing.T) {
 		t.Errorf("Unexpected error, got: %q, want: %q.", len(match), len(expectedContextsToReplace))
 	}
 }
+
+func Test_getHelmURL(t *testing.T) {
+	got := getHelmURL("amd64", "darwin", "v2.14.3")
+	want := "https://get.helm.sh/helm-v2.14.3-darwin-amd64.tar.gz"
+
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
