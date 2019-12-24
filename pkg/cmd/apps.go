3f69acd5437150c25a0907bce78b821f4c107749
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-12-24 13:46:54 +0530

    Add app info subcommand
    Add app info subcommand to show post install instructions
    for an app and the app homepage link, so that users don't
    have to unnecessarily install again to see them, which
    was the only way till now to see post install
    instructions.
    
    This commit adds info for openfaas and inlets-operator
    app
    
    Fixes: #133
    
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@gmail.com>

--- pkg/cmd/apps.go
+++ pkg/cmd/apps.go
@@ -9,10 +9,11 @@ import (
 
 func MakeApps() *cobra.Command {
 	var command = &cobra.Command{
-		Use:          "app",
-		Short:        "Manage Kubernetes apps",
-		Long:         `Manage Kubernetes apps`,
-		Example:      `  k3sup app install`,
+		Use:   "app",
+		Short: "Manage Kubernetes apps",
+		Long:  `Manage Kubernetes apps`,
+		Example: `  k3sup app install
+  k3sup app info`,
 		SilenceUsage: false,
 	}
 
@@ -38,6 +39,42 @@ func MakeApps() *cobra.Command {
 		return nil
 	}
 
+	info := &cobra.Command{
+		Use:   "info",
+		Short: "Find info about a Kubernetes app",
+		Long:  "Find info about how to use the installed Kubernetes app",
+		Example: `  k3sup app info [APP]
+  k3sup app info openfaas
+  k3sup app info inlets-operator
+  k3sup app info
+  k3sup app info --help`,
+		SilenceUsage: true,
+	}
+
+	info.RunE = func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			fmt.Println("You can get info about: openfaas, inlets-operator")
+			return nil
+		}
+
+		if len(args) != 1 {
+			return fmt.Errorf("you can only get info about exactly one installed app")
+		}
+
+		appName := args[0]
+
+		switch appName {
+		case "inlets-operator":
+			fmt.Println(inletsOperatorInfoMsg)
+		case "openfaas":
+			fmt.Println(openfaasInfoMsg)
+		default:
+			return fmt.Errorf("no info or no app available for %s", appName)
+		}
+
+		return nil
+	}
+
 	command.AddCommand(install)
 	install.AddCommand(makeInstallOpenFaaS())
 	install.AddCommand(makeInstallMetricsServer())
@@ -53,6 +90,8 @@ func MakeApps() *cobra.Command {
 	install.AddCommand(makeInstallMinio())
 	install.AddCommand(makeInstallPostgresql())
 	install.AddCommand(makeInstallKubernetesDashboard())
+
+	command.AddCommand(info)
 
 	return command
 }
