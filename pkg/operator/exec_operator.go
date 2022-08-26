8723f19643bdf388ee8e3bd2b8e7c98e81384ed9
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2022-08-26 11:48:32 +0100

    Remove old packages from apps feature
    K3sup used to have an app installation feature, which moved
    into arkade a long time ago. These packages are no longer
    used or required and can be removed.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ pkg/operator/exec_operator.go
@@ -0,0 +1,31 @@
+package ssh
+
+import (
+	goexecute "github.com/alexellis/go-execute/pkg/v1"
+)
+
+// ExecOperator executes commands on the local machine directly
+type ExecOperator struct {
+}
+
+func (ex ExecOperator) ExecuteStdio(command string, stream bool) (CommandRes, error) {
+	task := goexecute.ExecTask{
+		Command:     command,
+		Shell:       true,
+		StreamStdio: stream,
+	}
+
+	res, err := task.Execute()
+	if err != nil {
+		return CommandRes{}, err
+	}
+
+	return CommandRes{
+		StdErr: []byte(res.Stderr),
+		StdOut: []byte(res.Stdout),
+	}, nil
+}
+
+func (ex ExecOperator) Execute(command string) (CommandRes, error) {
+	return ex.ExecuteStdio(command, true)
+}
